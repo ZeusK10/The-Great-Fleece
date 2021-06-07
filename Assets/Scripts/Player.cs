dee0331ad5461ec0d7a27dc49b93fdeb71ad11f9
ZeusK10 <neeraz.karkee18@gmail.com>
2021-06-07 21:43:26 +0545

    Player movement created

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator _animator;
+    RaycastHit hitInfo;
+    Ray rayOrigin;
+    float dist;
+
+    void Start()
+    {
+        _animator = gameObject.GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(rayOrigin,out hitInfo))
+            {
+                agent.destination = hitInfo.point;
+                _animator.SetBool("Walk", true);
+            }
+        }
+        dist = Vector3.Distance(hitInfo.point, transform.position); 
+        if (dist<1)
+        {
+            _animator.SetBool("Walk", false);
+        }
+    }
+}
